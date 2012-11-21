83687be7d676aa46cc3b731ce4abad30a03808c3
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-21 11:23:27 -0800

    Ensure self is properly listed via discovery. Set properly.

--- recipes/discovery.rb
+++ recipes/discovery.rb
@@ -5,5 +5,11 @@ end
 zk_nodes = search(:node, zk_search)
 
 zk_nodes.each do |znode|
-  node[:zookeeperd][:config]["server.#{znode[:zookeeperd][:zk_id]}"] = "#{znode[:ipaddress]}:#{znode[:zookeeperd][:cluster][:follower_port]}:#{znode[:zookeeperd][:cluster][:election_port]}"
+  node.default[:zookeeperd][:config]["server.#{znode[:zookeeperd][:zk_id]}"] = "#{znode[:ipaddress]}:#{znode[:zookeeperd][:cluster][:follower_port]}:#{znode[:zookeeperd][:cluster][:election_port]}"
+end
+
+# ensure this node is in there (may not be on first run)
+
+unless(node[:zookeeperd][:config].include?("server.#{node[:zookeeperd][:zk_id]}"))
+  node.default[:zookeeperd][:config]["server.#{node[:zookeeperd][:zk_id]}"] = "#{node[:ipaddress]}:#{node[:zookeeperd][:cluster][:follower_port]}:#{node[:zookeeperd][:cluster][:election_port]}"
 end
