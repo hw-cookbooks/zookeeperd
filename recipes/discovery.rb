e3e5975aa7b0a92c21947e408e196c4061c68165
chrisroberts <chrisx.roberts@intel.com>
2013-06-06 09:34:30 -0700

    Update discovery for better attribute handling

--- recipes/discovery.rb
+++ recipes/discovery.rb
@@ -1,21 +1,32 @@
 zk_nodes = discovery_all(
-  'zk_id:*', 
+  'zk_id:* AND recipes:zookeeperd\:\:server', 
   :environment_aware => node[:zookeeperd][:cluster][:common_environment],
   :empty_ok => true,
   :minimum_response_time_sec => false,
-  :remove_self => false,
+  :remove_self => true,
   :raw_search => true
 )
 
 zk_hash = {}
+zk_nodes.push(node) if node[:zookeeperd][:zk_id]
 
 zk_nodes.each do |znode|
-  zk_hash["server.#{znode[:zookeeperd][:zk_id]}"] = "#{znode[:ipaddress]}:#{znode[:zookeeperd][:cluster][:follower_port]}:#{znode[:zookeeperd][:cluster][:election_port]}"
+  zinfo = [znode[:zookeeperd][:ipaddress]]
+  zinfo << znode[:zookeeperd][:cluster][:follower_port]
+  zinfo << znode[:zookeeperd][:cluster][:election_port]
+  zk_hash["server.#{znode[:zookeeperd][:zk_id]}"] = zinfo.join(':')
 end
 
 # ensure this node is in there (may not be on first run)
-
-unless(zk_hash.has_key?("server.#{node[:zookeeperd][:zk_id]}"))
-  zk_hash["server.#{node[:zookeeperd][:zk_id]}"] = "#{node[:ipaddress]}:#{node[:zookeeperd][:cluster][:follower_port]}:#{node[:zookeeperd][:cluster][:election_port]}"
+if(node[:zookeeperd][:zk_id])
+  unless(zk_hash.has_key?("server.#{node[:zookeeperd][:zk_id]}"))
+    zinfo = [node[:zookeeperd][:ipaddress]]
+    zinfo << node[:zookeeperd][:cluster][:follower_port]
+    zinfo << node[:zookeeperd][:cluster][:election_port]
+    zk_hash["server.#{node[:zookeeperd][:zk_id]}"] = zinfo.join(':')
+  end
 end
-node.set[:zookeeperd][:config] = zk_hash
+
+zk_hash.each do |k,v|
+  node.set[:zookeeperd][:config][k] = v
+end
