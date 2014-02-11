9b1cde1dc8ed434b97fa2719cffa142821ba7c21
ianrossi <ian.d.rossi@aimtheory.com>
2014-02-11 14:35:41 -0500

    Controlling cloudera bits with a node attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ if(node.platform_family?('rhel', 'fedora', 'suse'))
   default[:zookeeperd][:server_packages] = %w(zookeeper-server)
   default[:zookeeperd][:client_packages] = %w(zookeeper)
   default[:zookeeperd][:service_name] = 'zookeeper-server'
+  default[:zookeeperd][:cloudera_repo] = true
 elsif(node.platform_family?('debian'))
   default[:zookeeperd][:server_packages] = %w(zookeeperd)
   default[:zookeeperd][:client_packages] = %w(zookeeper)
@@ -12,6 +13,3 @@ default[:zookeeperd][:zk_id] = nil
 default[:zookeeperd][:auto_id] = 'hostid'
 default[:zookeeperd][:ipaddress] = node.ipaddress
 default[:zookeeperd][:int_bit_limit] = 32
-
-#default[:java][:install_flavor] = "openjdk"
-#default[:java][:jdk_version] = "6"
