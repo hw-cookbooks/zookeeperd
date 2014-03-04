dc9274ee0e47582b42c479adf9987b549a4a651d
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-03-04 14:41:26 -0500

    Final changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ if(node.platform_family?('rhel', 'fedora', 'suse'))
   default[:zookeeperd][:client_packages] = %w(zookeeper)
   default[:zookeeperd][:service_name] = 'zookeeper-server'
   default[:zookeeperd][:cloudera_repo] = true
-  default[:zookeeperd][:cloudera][:baseurl] = "http://archive.cloudera.com/cdh4/#{node[:languages][:ruby][:target_vendor]}/#{node[:platform_version].to_f.floor}/#{node[:kernel][:machine]}/cdh/4/"
-  default[:zookeeperd][:cloudera][:gpgkey] = "http://archive.cloudera.com/cdh4/#{node[:languages][:ruby][:target_vendor]}/#{node[:platform_version].to_f.floor}/#{node[:kernel][:machine]}/cdh/RPM-GPG-KEY-cloudera"
+  default[:zookeeperd][:cloudera][:baseurl] = "http://archive.cloudera.com/cdh4/redhat/#{node[:platform_version].to_i}/#{node[:kernel][:machine]}/cdh/4/"
+  default[:zookeeperd][:cloudera][:gpgkey] = "http://archive.cloudera.com/cdh4/redhat/#{node[:platform_version].to_i}/#{node[:kernel][:machine]}/cdh/RPM-GPG-KEY-cloudera"
 elsif(node.platform_family?('debian'))
   default[:zookeeperd][:server_packages] = %w(zookeeperd)
   default[:zookeeperd][:client_packages] = %w(zookeeper)
