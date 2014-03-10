b21ed627c43a820d8a7750d4b5d0510d30e572cd
Chris Roberts <chrisroberts.code@gmail.com>
2014-03-10 10:58:35 -0700

    Update files for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.6
+* Adds support for RHEL platform (thanks @ianrossi!)
+* Adds base test kitchen support
+
 ## v0.1.2
 * Add attribute based size limit tuning for ids
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'support@hw-ops.com'
 license 'Apache 2.0'
 
 name 'zookeeperd'
-version '0.1.5'
+version '0.1.6'
 
 depends 'apt'
 depends 'discovery', '>= 0.2.0'
