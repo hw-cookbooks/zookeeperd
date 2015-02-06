b4aa898646b7975c3be676353fd13ff6f525f027
Cameron Johnston <cameron@rootdown.net>
2015-02-05 23:13:07 -0700

    wire up jackal-kitchen

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -8,4 +8,8 @@ RSpec.configure do |c|
   c.before :all do
     c.path = "/sbin:/usr/sbin"
   end
+  output_dir = '/tmp/output'
+  Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
+  c.output_stream = File.open(File.join(output_dir,'serverspec.json'), 'w')
+  c.formatter = 'json'
 end
