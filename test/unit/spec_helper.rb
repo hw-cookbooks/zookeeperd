026eaecccee6a1a82c317646cd2054c12f22e835
Cameron Johnston <cameron@rootdown.net>
2015-05-20 10:22:31 -0600

    Revert "statically set chefspec results output dir"
    This reverts commit 18de6ff6ae6c7ba71cdd403082cc9fc14502356f.

--- test/unit/spec_helper.rb
+++ test/unit/spec_helper.rb
@@ -4,8 +4,8 @@ require_relative "support/matchers"
 
 RSpec.configure do |config|
   config.log_level = :fatal
-  output_dir = '/tmp'
-  # Dir.new(output_dir)
+  output_dir = 'output'
+  Dir.new(output_dir)
   config.output_stream = File.open(File.join(output_dir,'chefspec.json'), 'w')
   config.formatter = 'json'
 end
