15a6c39c1af262e54029d9d3b5a247930ee053d4
Michael F. Weinberg <michael@luckymike.com>
2016-01-29 16:58:12 -0800

    Increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'support@hw-ops.com'
 license 'Apache 2.0'
 
 name 'zookeeperd'
-version '0.2.8'
+version '0.2.9'
 description 'Installs and configures Apache Zookeeper'
 
 depends 'apt'
